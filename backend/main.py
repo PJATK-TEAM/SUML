0a9a01583c87e1c3d9e912a253091aa3ba2dc27d
przemyslawren <przemyslaw.ren@gmail.com>
2025-04-02 21:38:55 +0200

    feat: add validation

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
+import random
+
 from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI(title="Bird Drone Classifier API", version="1.0.0")
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
 @app.get("/")
 def read_root():
@@ -8,4 +11,10 @@ def read_root():
 
 @app.post("/classify")
 async def classify(file: UploadFile = File(...)):
-    return {"filename": file.filename, "content_type": file.content_type}
+    if file.content_type not in ["image/jpg","image/jpeg", "image/png"]:
+        return {"error": "Invalid file type. Only JPEG and PNG files are allowed."}
+    if file.size > MAX_FILE_SIZE:
+        return {"error": "File too large. Maximum size is 5MB."}
+
+    possible_results = ["Bird", "Drone"]
+    return {"message": random.choice(possible_results)}
